7bbead60b2fb33a1ea16c0f3b667bdcb62457f57
FardinAhsan146 <69723973+FardinAhsan146@users.noreply.github.com>
2021-03-10 02:50:34 -0800

    Update approximate.py

--- approximate.py
+++ approximate.py
@@ -1,4 +1,5 @@
 
+
 import mip
 from typing import List
 import re
@@ -10,7 +11,7 @@ def equivalent_tol(components: List[float], target: float, series: bool, toleran
      of the target value, minimising the number of components in use.
 
     Args:
-        components: float values of the resistors/capacitors to choose from. A resistor
+        components: float values of the resistors/capacitors to choose from. A component
             value can be used as many times as it occurs in this list.
         target: The target value.
         series: True for series, false for parallel.
@@ -60,13 +61,11 @@ def equivalent_tol(components: List[float], target: float, series: bool, toleran
     r_to_use = [r for r, i in zip(components, r_in_use_sol) if i > 0]
     
     if resistor:
-        solved_values = sum(x for x in r_to_use) if series\
-            else 1/sum(1/x for x in r_to_use)
+        solved_values = sum(x for x in r_to_use) if series else 1/sum(1/x for x in r_to_use)
         solved_error = 100 * (solved_values - target) / target
         
     else:
-        solved_values = sum(x for x in r_to_use) if not series\
-            else 1/sum(1/x for x in r_to_use)
+        solved_values = sum(x for x in r_to_use) if not series else 1/sum(1/x for x in r_to_use)
         solved_error = 100 * (solved_values - target) / target
         
     
@@ -78,13 +77,36 @@ def equivalent_tol(components: List[float], target: float, series: bool, toleran
     return r_to_use
 
 
+
+def list_precoressing(raw_list: List[str]):
+    """
+    Preprocess lists of strings.
+    
+    should turn the list ["5kxn"] into [5*1e3 for i in range(len(n))] for example
+    
+    """
+
+    replace_dict = {'a': '*1e-18', 'f': '*1e-15', 'p': '*1e-12',
+                    'n':   '*1e-9','u': '*1e-6',  'm': '*1e-3',
+                    'c':  '*1e-2', 'd': '*1e-1', 'da': '*1e1',
+                    'h':   '*1e2', 'k': '*1e3',   'M': '*1e6',
+                    'G':   '*1e9', 'T': '*1e12',  "P": '*1e15',
+                    'E': '*1e18'}
+    
+    data = {'Val': raw_list}
+    df = pd.DataFrame(data = data)
+    df['Val'] = df['Val'].replace(replace_dict, regex=True).map(pd.eval).astype(float)
+    return df['Val'].tolist()
+
+    #now we need to do something about the xn part of the input
+    
 if __name__ == "__main__":
     
-    #df['Val'].replace({'K': '*1e3', 'M': '*1e6', 'm': '*1e-3', 'u': '*1e-6', 'n': '*1e-9', 'p': '*1e-12', 'f': , 
-                   #}, regex=True).map(pd.eval).astype(float)
+
 
     sol = equivalent_tol([1, 2, 3, 4, 5, 6, 7], 11, True, 10,True)
     sol = equivalent_tol([1, 2, 3, 4, 5, 6, 7], 11, False, 10,False)
+
 
 
     
