308f7de41ea58a83b4bfdbd3069f034cd20be00d
FardinAhsan146 <fardinahsan146@gmail.com>
2021-08-23 03:19:56 +0400

    refactor

--- approximate.py
+++ approximate.py
@@ -3,9 +3,9 @@ from typing import List
 import re
 
 def equivalent_tol(components: List[float], target: float, series: bool, tolerance: float, resistor: bool) -> List[float]:
+    
     """Return list of components which in series/parallel are within tolerance
      of the target value, minimising the number of components in use.
-
     Args:
         components: float values of the resistors/capacitors to choose from. A component
             value can be used as many times as it occurs in this list.
@@ -13,61 +13,46 @@ def equivalent_tol(components: List[float], target: float, series: bool, toleran
         series: True for series, false for parallel.
         tol: Solved resistance will be in range [(1-tol)*target, (1+tol)*target, %
         resistor: True for resistors and inductors, False for capacitors
-
     Returns:
         Optimal component values, or empty list if no solution.
     """
     
     tol = tolerance/100
     
-    if resistor:
-        
-        _target = target if series else 1/target
-        _components = components if series else [1/x for x in components]
-        lower = (1-tol) * target if series else 1/((1+tol) * target)
-        upper = (1+tol) * target if series else 1/((1-tol) * target)
-        
-    else:
-        
-        _target = target if not series else 1/target
-        _components = components if not series else [1/x for x in components]
-        lower = (1-tol) * target if not series else 1/((1+tol) * target)
-        upper = (1+tol) * target if not series else 1/((1-tol) * target)
+    # This is what sets the different parallel and series combination 
+    # depending on the component type. Reasoning = Boolean algebra.
+    condition = (not resistor and not series) or (series and resistor)
+    _target = target if condition else 1/target
+    _components = components if condition else [1/x for x in components]
+    lower = (1-tol) * target if condition else 1/((1+tol) * target)
+    upper = (1+tol) * target if condition else 1/((1-tol) * target)
+
                
     m = mip.Model()  # Create new mixed integer/linear model.
-
 
     r_in_use = [m.add_var(var_type=mip.BINARY) for _ in _components]
     opt_r = sum([b * r for b, r in zip(r_in_use, _components)])  
     m += opt_r >= lower
     m += opt_r <= upper
 
-
     m.objective = mip.minimize(mip.xsum(r_in_use))
     m.verbose = False  
     sol_status = m.optimize()
     if sol_status != mip.OptimizationStatus.OPTIMAL:
-        print(f'No solution found')
+        print('No solution found')
         return []
 
 
     r_in_use_sol = [float(v) for v in r_in_use]
-
-
     r_to_use = [r for r, i in zip(components, r_in_use_sol) if i > 0]
     
-    if resistor:
-        solved_values = sum(x for x in r_to_use) if series else 1/sum(1/x for x in r_to_use)
-        solved_error = 100 * (solved_values - target) / target
+    solved_values = sum(x for x in r_to_use) if condition else 1/sum(1/x for x in r_to_use)
+    solved_error = 100 * (solved_values - target) / target
         
-    else:
-        solved_values = sum(x for x in r_to_use) if not series else 1/sum(1/x for x in r_to_use)
-        solved_error = 100 * (solved_values - target) / target
-        
-    
-    print(f'{"Resistors" if resistor else "Capacitors"} {r_to_use} in {"series" if series else "parallel"} '
+    print(f'{"Resistors/Inductors" if resistor else "Capacitors"}; {r_to_use} in {"series" if series else "parallel"} '
           
-          f'will produce {"resistance" if resistor else "capacitance"} = {solved_values:.3f}. Aiming for {"R" if resistor else "C"} = {target:.3f}, '
+          f'will produce {"resistance/inductance" if resistor else "capacitance"} = {solved_values:.3f}'\
+          f' {"R/I" if resistor else "C"}. Aiming for {"R/I" if resistor else "C"} = {target:.3f}, '
           
           f'error of {solved_error:.2f}%')
     return r_to_use
@@ -101,44 +86,14 @@ def list_process(list_str: List[str]):
     
 if __name__ == "__main__":
     
-    """
-    Run this code block to test.
+      
+    # Run this code block to test.
     
     processed_list = list_process(["1kx5"])
     
+    print()
     sol = equivalent_tol(processed_list, 5000, True, 10,True)
+    print()
     sol = equivalent_tol([1, 2, 3, 4, 5, 6, 7], 11, True, 10,True)
+    print()
     sol = equivalent_tol([1, 2, 3, 4, 5, 6, 7], 11, False, 10,False)
-    """
-
-
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
